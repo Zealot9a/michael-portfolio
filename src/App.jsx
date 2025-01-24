5beb57198d6b1d07a571a45358473d09055c7407
Michael <zealot9a@gmail.com>
2025-01-24 18:23:03 +0100

    "Project Re-Setup"

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <h2>Portfolio website</h2>
+  )
+}
+
+export default App
